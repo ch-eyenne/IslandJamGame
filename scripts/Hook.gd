d3065db71bcf7003d5485fa1cbda1de3fd12d7bb
ch-eyenne <24706428+ch-eyenne@users.noreply.github.com>
2023-04-15 13:38:03 -1000

    Revert to f56dfe5”

--- scripts/Hook.gd
+++ scripts/Hook.gd
@@ -3,28 +3,12 @@ extends CharacterBody2D
 @onready var is_active = false
 @onready var player = $"../Player"
 
-<<<<<<< HEAD
-var is_sticked = false
-=======
->>>>>>> bcaf5fe8bfdaaa628fa0c4f4dbfb03329f77504e
 var new_position
 func _ready():
 	add_collision_exception_with(player)
 	pass
 
 func _physics_process(dt: float) -> void:
-<<<<<<< HEAD
-	if self.is_active and !self.is_sticked and position != new_position:
-		velocity = (lerp(position, new_position, .3) - position) /dt
-		move_and_slide()
-	if !self.is_active and !self.is_sticked:
-		self.position = player.position
-		pass
-	if self.is_sticked:
-		self.velocity = Vector2.ZERO
-		pass
-	if !self.is_active:
-=======
 	if self.is_active and position != new_position:
 		velocity = (lerp(position, new_position, .100) - position) /dt
 		move_and_slide()
@@ -35,7 +19,6 @@ func _physics_process(dt: float) -> void:
 		pass
 	
 	if self.is_active == false:
->>>>>>> bcaf5fe8bfdaaa628fa0c4f4dbfb03329f77504e
 		#self.visible = false
 		pass
 	if self.is_active:
@@ -44,20 +27,11 @@ func _physics_process(dt: float) -> void:
 
 func hook_shot():
 	new_position = get_global_mouse_position()
-<<<<<<< HEAD
-	self.is_sticked = false
-=======
->>>>>>> bcaf5fe8bfdaaa628fa0c4f4dbfb03329f77504e
 	pass
 
 func reset():
 	self.position = player.position
 	self.is_active = false
-<<<<<<< HEAD
-	self.is_sticked = false
-	player.is_hooked = false
-=======
->>>>>>> bcaf5fe8bfdaaa628fa0c4f4dbfb03329f77504e
 	pass
 
 
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -2,24 +2,18 @@ extends CharacterBody2D
 
 
 @export var speed = 400
-@export var jump_speed = -500
+@export var jump_speed = -350
 @export var acceleration = 15
 @export var friction = 15
 
 @onready var hook  = $"../Hook"
 @onready var hook_active_timer = $"../Hook/hook_active_timer"
-<<<<<<< HEAD
-var direction
-var mousepoint
-var is_hooked
-=======
 
 var mousepoint
->>>>>>> bcaf5fe8bfdaaa628fa0c4f4dbfb03329f77504e
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func get_input(delta):
-	direction = Input.get_axis("left", "right")
+	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = lerp(velocity.x, direction * speed, acceleration * delta)
 	else:
@@ -27,18 +21,6 @@ func get_input(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_speed
 	
-<<<<<<< HEAD
-func _physics_process(dt):
-	if is_hooked:
-		velocity.y = (lerp(position.y, hook.position.y, .03) - position.y) /dt
-		velocity.x = lerp(velocity.x, direction * speed, acceleration * dt)
-		move_and_slide()
-	else:
-		velocity.y += gravity * dt
-		get_input(dt)
-		move_and_slide()
-# hook
-=======
 func _physics_process(delta):
 	velocity.y += gravity * delta
 	get_input(delta)
@@ -47,7 +29,6 @@ func _physics_process(delta):
 
 # hook
 
->>>>>>> bcaf5fe8bfdaaa628fa0c4f4dbfb03329f77504e
 func _unhandled_input(event):
 	if Input.is_action_just_pressed("click") and !hook.is_active:
 		hook.is_active = true
@@ -58,17 +39,9 @@ func _unhandled_input(event):
 
 func _on_area_2d_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
 	if hook.is_active:
-<<<<<<< HEAD
-		hook.is_sticked = true
-		self.is_hooked = true
-		hook_active_timer.start()
-		pass
-	pass 
-=======
 		hook_active_timer.start()
 		pass
 	pass # Replace with function body.
->>>>>>> bcaf5fe8bfdaaa628fa0c4f4dbfb03329f77504e
 
 func reset_hook():
 		hook.reset()
